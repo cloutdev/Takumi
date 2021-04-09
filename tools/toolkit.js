b534b3cb7e6ebc916878a06caebdaea8ea54b638
alphadev <pan.kalog16@gmail.com>
2021-04-09 03:32:23 +0300

    Added closeChannel function.

---
+++ tools/toolkit.js
@@ -0,0 +1,33 @@
+const db = require("../tools/database");
+const {QueryTypes} = require('sequelize');
+
+async function closeChannel(channelToBeClosed, discordClient){
+
+	const guild = (await db.query("SELECT * from settings WHERE guildID = ?",{
+		replacements: [channelToBeClosed.guild.id],
+		type: QueryTypes.SELECT,
+		logging: false,
+	}))[0];
+
+	const dbChannel = (await db.query("SELECT * from channels WHERE channelID = ?",{
+		replacements: [channelToBeClosed.id],
+		type: QueryTypes.SELECT,
+		logging: false,
+	}))[0];
+
+	db.query('UPDATE channels SET isClosed = 1 WHERE channelID = ?',{
+		replacements: [channelToBeClosed.id],
+		type: QueryTypes.UPDATE,
+		logging: false,
+	}).then(()=>{
+		channelToBeClosed.setParent(guild.closedCategoryID);
+		discordClient.channels.fetch(guild.privateBotChannel).then((channel)=>{
+			channel.send(channelToBeClosed.name+" Moved to Closed Channels!");
+		});
+		discordClient.users.fetch(dbChannel.masterUser).then((user)=>{
+			user.send(`Hello! You had purchased a channel at the server \`${channelToBeClosed.guild.name}\`, but since you did not renew your subscription for that channel, we are afraid that the channel with name \`${channelToBeClosed.name}\` was closed. You can reopen the channel, as we did not delete it.`);
+		});
+	});
+}
+
+module.exports.closeChannel = closeChannel;
