a88609c28864dea6d686561202a3478e966688ab
alphadev <pan.kalog16@gmail.com>
2021-04-20 10:26:24 +0300

    Moved channel creation method to another file

--- commands/Testing/createChannel.js
+++ commands/Testing/createChannel.js
@@ -1,5 +1,4 @@
-const db = require("../../tools/database.js");
-const {QueryTypes} = require('sequelize');
+const toolkit = require("../../tools/toolkit");
 //Here the command starts
 module.exports = {
     //definition
@@ -13,62 +12,6 @@ module.exports = {
     //running the command with the parameters: client, message, args, user, text, prefix '581575466578870302', '675476898926559239'
     // eslint-disable-next-line no-unused-vars
     run: async (client, message, args, user, text, prefix) => {
-		const guild = (await db.query("SELECT * from settings WHERE guildID = ?",{
-			replacements: [message.guild.id],
-			type: QueryTypes.SELECT,
-		}))[0];
-
-		const channelName = "Test1";
-		const channelDesc = "GAyyyyyyyyyy";
-		
-		const testArr = [];
-
-		let permsArray = [
-			{
-				id: user.id,
-				type: 'member',
-				allow: [
-					['MANAGE_CHANNELS', 'SEND_MESSAGES']
-				]
-			}
-		];
-		console.log(permsArray);
-		console.log(guild.openCategoryID);
-		const createdChannelID = await message.guild.channels.create(channelName,{
-			topic : channelDesc,
-			parent: guild.openCategoryID,
-		}).then((channel)=>{
-			channel.updateOverwrite(user.id, 
-				{
-					'MANAGE_CHANNELS': true,
-					'SEND_MESSAGES': true,
-					'MANAGE_MESSAGES': true,
-					'EMBED_LINKS': true,
-					'ATTACH_FILES': true,
-				}
-			);
-			testArr.forEach((modID) => {
-				channel.updateOverwrite(modID, {
-					'MANAGE_MESSAGES': true,
-					'EMBED_LINKS' : true,
-				})
-			});
-			return channel.id;
-		}).catch(()=>{
-			message.reply("Something went wrong. Please try again later.");
-			return;
-		});
-
-		console.log(
-			await db.query('INSERT INTO channels (guildID, channelID, createdBy, masterUser, startsOn, expiresOn) values (?, ?, ?, ?, now(), DATE_ADD(now(), INTERVAL ? MINUTE))',{
-				replacements: [message.guild.id, createdChannelID, user.id, user.id, 1],
-				type: QueryTypes.INSERT,
-				logging: console.log,
-			})
-		);
-
-		console.log(
-			await db.query("SELECT * from channels where expiresOn >= now()")
-		);
+		toolkit.createChannel(client, message.guild.id, user.id, user.id);
     }
 }
