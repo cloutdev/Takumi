55885eb8b10b574483ea8cd949687b7e17ba3e22
CloutDev <pan.kalog16@gmail.com>
2021-05-12 10:44:54 +0300

    Channel Information Command

---
+++ commands/Testing/showChannelInfo.js
@@ -0,0 +1,18 @@
+//Here the command starts
+module.exports = {
+    //definition
+    name: "say", //the name of the command 
+    category: "fun", //the category this will be listed at, for the help cmd
+    aliases: ["say", "sayit"], //every parameter can be an alias
+    cooldown: 2, //this will set it to a 2 second cooldown
+    usage: "say <Text>", //this is for the help command for EACH cmd
+    description: "Resends the message", //the description of the command
+
+    //running the command with the parameters: client, message, args, user, text, prefix
+    // eslint-disable-next-line no-unused-vars
+    run: async (client, message, args, user, text, prefix) => {
+
+        
+
+    }
+}
