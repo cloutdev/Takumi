e1639568e36f80d3593da474a34c79c102a28676
CloutDev <pan.kalog16@gmail.com>
2021-05-13 22:52:23 +0300

    Converted to Deprecated and updated.

--- commands/Testing/renewStore.js
+++ commands/Testing/renewStore.js
@@ -7,9 +7,9 @@ module.exports = {
     name: "renewStore", //the name of the command 
     category: "Testing", //the category this will be listed at, for the help cmd
     aliases: ["renew"], //every parameter can be an alias
-    cooldown: 2, //this will set it to a 2 second cooldown
+    cooldown: 2, //this will set it to a 2 second cool  down
     usage: "renewStore", //this is for the help command for EACH cmd
-    description: "Used to renew a store's subscription.", //the description of the command
+    description: "Used to renew a store's subscription. DEPRECATED, ONLY USE THE INFORMATION MENU", //the description of the command
 
     //running the command with the parameters: client, message, args, user, text, prefix
     // eslint-disable-next-line no-unused-vars
@@ -25,14 +25,11 @@ module.exports = {
       const successfulInputEmbed = new Discord.MessageEmbed()
         .setColor('#0099ff')
         .setTitle('Thanks for your inputs!')
-        .setDescription('Using your inputs, the guild ID, and your User ID, we will now make some behind-the-scenes magic to deliver you a Sellix payment invoice.\n\n'+
-        'When that invoice is paid, with any of the payment methods you would like, we will automatically renew the channel to which you sent the initial command.\n\n'+
-        'We need to clarify that, for simplicity, safety, and ease of use, we are using Sellix.io as our payment processor. That means that, we do not receive any information pertraining to your payment information, including possibly credit card info or PayPal credentials. We only receive information relevant to the invoice that has been paid, not to **how** it has been paid.\n\n'+
-        'Below you will find the information we will send to Sellix to make your payment, and the subsequent Store creation. If you want to, you can use them to verify the information you provided.') 
+        .setDescription('Using your inputs, the guild ID, and your User ID, we will now make some behind-the-scenes magic to deliver you a Shoppy payment invoice.\n\n')
         .addFields(
           { name: 'Channel ID (Your store\'s ID)', value: message.channel.id, inline: true },
           { name: 'Guild ID', value: message.guild.id, inline: true },
-          )
+        )
           
       user.send(successfulInputEmbed);
 
@@ -44,7 +41,7 @@ module.exports = {
       else{
         const successfulWebhookReceivedEmbed = new Discord.MessageEmbed()
           .setColor('#0099ff')
-          .setTitle('Press here to be taken to the Sellix invoice page!')
+          .setTitle('Press here to be taken to the Shoppy invoice page!')
           .setDescription('When we receive your payment, we will renew your shop, with the days of subscription time that you requested.')
           .setURL(shoppyResponseBody);
         
