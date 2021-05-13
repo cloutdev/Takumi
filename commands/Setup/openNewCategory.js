725d5ba64192833d46178af96db99b5e78431f1f
CloutDev <pan.kalog16@gmail.com>
2021-05-13 22:50:20 +0300

    Edited Metadata

--- commands/Setup/openNewCategory.js
+++ commands/Setup/openNewCategory.js
@@ -7,7 +7,7 @@ module.exports = {
 	//definition
 	name: "create", //the name of the command 
 	category: "fun", //the category this will be listed at, for the help cmd
-	aliases: ["say", "sayit"], //every parameter can be an alias
+	aliases: ["say", "sayit", "category"], //every parameter can be an alias
 	cooldown: 2, //this will set it to a 2 second cooldown
 	usage: "say <Text>", //this is for the help command for EACH cmd
 	description: "Resends the message", //the description of the command
