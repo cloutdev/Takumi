5c4cd65c73993316e57e951d8d0f0ba4f0d9cb37
CloutDev <pan.kalog16@gmail.com>
2021-05-14 15:07:34 +0300

    Quick removal command to make the bot not look for that guild

---
+++ commands/Setup/removeBot.js
@@ -0,0 +1,41 @@
+const prisma = require("../../tools/prisma")
+
+//Here the command starts
+module.exports = {
+    //definition
+    name: "remove", //the name of the command 
+    category: "fun", //the category this will be listed at, for the help cmd
+    aliases: [], //every parameter can be an alias
+    cooldown: 2, //this will set it to a 2 second cooldown
+    usage: "say <Text>", //this is for the help command for EACH cmd
+    description: "Resends the message", //the description of the command
+
+    //running the command with the parameters: client, message, args, user, text, prefix
+    // eslint-disable-next-line no-unused-vars
+    run: async (client, message, args, user, text, prefix) => {
+        if (message.guild.ownerID !== user.id){console.log("no perms")}
+        
+        await prisma.settings.update({
+            where: {
+                guildID: message.guild.id
+            },
+            data:{
+                isActive: false
+            }
+        })
+
+        await prisma.channels.updateMany({
+            where: {
+                guildID: message.guild.id
+            },
+            data:{
+                isClosed: true
+            }
+        })
+
+
+        console.log("success")
+
+
+    }
+}
